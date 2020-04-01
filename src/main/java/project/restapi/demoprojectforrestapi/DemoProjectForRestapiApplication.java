d8534f006a763459a85b4a562ce5d577c3ae161d
freeefly <freefly3557@gmail.com>
2020-04-01 23:33:49 +0900

    feat : first commit

---
+++ src/main/java/project/restapi/demoprojectforrestapi/DemoProjectForRestapiApplication.java
@@ -0,0 +1,13 @@
+package project.restapi.demoprojectforrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectForRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/restapi/demoprojectforrestapi/DemoProjectForRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.restapi.demoprojectforrestapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
