3f97ac638af0a043085ee628dbfd2906ed92240b
freeefly <freefly3557@gmail.com>
2020-04-12 01:08:47 +0900

    fix : fix wrong test

--- src/test/java/project/restapi/demoprojectforrestapi/DemoProjectForRestapiApplicationTests.java
+++ src/test/java/project/restapi/demoprojectforrestapi/DemoProjectForRestapiApplicationTests.java
@@ -1,13 +1,18 @@
 package project.restapi.demoprojectforrestapi;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
-class DemoProjectForRestapiApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class DemoProjectForRestapiApplicationTests {
 
-    void contextLoads() {
+    @Test
+    public void contextLoads() {
     }
 
 }
